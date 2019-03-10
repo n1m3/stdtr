8bfebf4c94c47fb172f87c9daad11da266eaa1d9
Nils Mehrtens <nils@hasinila.de>
2019-03-10 22:18:51 +0100

    Initial commit

---
+++ cephes-sys/build.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::path::PathBuf;
+
+use cc::Build;
+
+fn main() {
+    let files = "acosh.c airy.c asin.c asinh.c atan.c atanh.c bdtr.c beta.c \
+                 btdtr.c cbrt.c chbevl.c chdtr.c clog.c cmplx.c const.c \
+                 cosh.c dawsn.c drand.c ei.c ellie.c ellik.c ellpe.c ellpj.c ellpk.c \
+                 exp.c exp10.c exp2.c expn.c expx2.c fabs.c fac.c fdtr.c \
+                 fresnl.c gamma.c gdtr.c hyp2f1.c hyperg.c i0.c i1.c igami.c incbet.c \
+                 incbi.c igam.c isnan.c iv.c j0.c j1.c jn.c jv.c k0.c k1.c kn.c kolmogorov.c \
+                 log.c log2.c log10.c lrand.c nbdtr.c ndtr.c ndtri.c pdtr.c planck.c \
+                 polevl.c polmisc.c polylog.c polyn.c pow.c powi.c psi.c rgamma.c round.c \
+                 shichi.c sici.c sin.c sindg.c sinh.c spence.c stdtr.c struve.c \
+                 tan.c tandg.c tanh.c unity.c yn.c zeta.c zetac.c \
+                 sqrt.c floor.c setprec.c mtherr.c";
+    let files = files.split(' ');
+    let mut build = Build::new();
+    let cephes_dir = "cephes";
+    build.include(cephes_dir);
+    for file in files {
+        let path = format!("{}/{}", cephes_dir, file);
+        dbg!(&path);
+        build.file(path);
+    }
+    build.flag_if_supported("-fno-builtin");
+    build.warnings(false);
+    build.opt_level(2);
+    build.compile("cephes");
+
+    let bindings = bindgen::Builder::default()
+        .header("cephes/cephes.h")
+        .generate()
+        .expect("Generating bindings failed");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use failure::{ensure, Error};
+
+use cephes_sys as ffi;
+
+/// Computes the integral from minus infinity to t of the Student
+/// t distribution with integer k > 0 degrees of freedom
+///
+///
+/// # Panics
+///
+/// if `k <= 0`
+pub fn unchecked_stdr(k: i32, t: f64) -> f64 {
+    checked_stdtr(k, t).unwrap()
+}
+/// Computes the integral from minus infinity to t of the Student
+/// t distribution with integer k > 0 degrees of freedom
+///
+///
+/// # Errors
+///
+/// if `k <= 0`
+pub fn checked_stdtr(k: i32, t: f64) -> Result<f64, Error> {
+    ensure!(k > 0, "k must be greater than 0.");
+    Ok(unsafe { ffi::stdtr(k, t) })
+}
+/// Returns incomplete beta integral of the arguments, evaluated
+/// from zero to x
+///
+/// # Panics
+///
+/// if `a <= 0` or `b <= 0` or `x < 0` or `x > 1`
+pub fn unchecked_inc_beta(a: f64, b: f64, x: f64) -> f64 {
+    checked_inc_beta(a, b, x).unwrap()
+}
+/// Returns incomplete beta integral of the arguments, evaluated
+/// from zero to x
+///
+/// # Errors
+///
+/// if `a <= 0` or `b <= 0` or `x < 0` or `x > 1`
+pub fn checked_inc_beta(a: f64, b: f64, x: f64) -> Result<f64, Error> {
+    ensure!(a > 0. && b > 0., "a and b must be greater than 0.");
+    ensure!(x >= 0., "x may not be smaller than 0.");
+    ensure!(x <= 1.0, "x may not be greater than 1.");
+    Ok(unsafe { ffi::incbet(a, b, x) })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn assert_almost_equal(a: f64, b: f64) {
+        if (a - b).abs() > 1.0E-14 {
+            panic!("{:?} vs {:?}", a, b);
+        }
+    }
+    #[test]
+    fn test_inc_bet() {
+        assert_eq!(unchecked_inc_beta(1.0, 2.0, 0.0), 0.0);
+        assert_eq!(unchecked_inc_beta(1.0, 2.0, 1.0), 1.0);
+        assert_almost_equal(unchecked_inc_beta(1.0, 2.0, 0.2), 0.36);
+        assert_almost_equal(unchecked_inc_beta(5.0, 2.0, 0.5), 0.109375);
+        assert_almost_equal(unchecked_inc_beta(1.0, 3.0, 0.6), 0.9359999999999999);
+        assert_almost_equal(unchecked_inc_beta(4.0, 3.0, 0.6), 0.54432);
+        assert_almost_equal(unchecked_inc_beta(2.0, 3.0, 0.5), 0.6875);
+    }
+}
